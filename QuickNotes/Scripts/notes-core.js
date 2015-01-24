44868e41cd7ac195f95d93d47c4aa92c41c440f6
Karthik Ananthapadmanaban <karthika25@gmail.com>
2015-01-23 23:11:46 -0500

    working copy!

--- QuickNotes/Scripts/notes-core.js
+++ QuickNotes/Scripts/notes-core.js
@@ -55,4 +55,20 @@ mainModule.controller("NotesController", function ($scope, $http, $timeout) {
 
         });
     };
+
+    $scope.isFlashAvailable = function() {
+        var hasFlash = false;
+        try {
+            var fo = new ActiveXObject('ShockwaveFlash.ShockwaveFlash');
+            if (fo) {
+                hasFlash = true;
+            }
+        }
+        catch (e) {
+            if (navigator.mimeTypes["application/x-shockwave-flash"] != undefined) {
+                hasFlash = true;
+            }
+        }
+        return hasFlash;
+    };
 });
